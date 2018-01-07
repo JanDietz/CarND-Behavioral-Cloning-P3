7932763913cd73022fb3005e66824000d6b6c026
Jan Dietz <jan.dietz02@gmail.com>
2018-01-07 22:22:21 +0100

    generator implemented

--- model.py
+++ model.py
@@ -1,48 +1,65 @@
+import os
 import csv
-import cv2
-import numpy as np
 
-lines = []
+# Read the csv-file to get all image pathes and steering angles
+samples = []
 with open('C:/Users/Jan/Desktop/sim_data_one_round/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
-    for line in reader:       
-        lines.append(line)
+    for line in reader:
+        samples.append(line)
 
-images = []
-measurements = []
-correction = 0.2
-image_sizeX = 320
-image_sizeY = 160
+from sklearn.model_selection import train_test_split
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
-def read_image(path,sizeX,sizeY):
-	bgr_image = cv2.imread(path)
-	bgr_image = cv2.resize(bgr_image,(sizeX, sizeY))
+import cv2
+import numpy as np
+import sklearn
+
+correction = 0.2  # correction value for steering angle
+
+def bgr2rgb(bgr_image):
 	b,g,r = cv2.split(bgr_image)       # get b,g,r
-	rgb_image = cv2.merge([r,g,b])       # switch it to rgb
+	rgb_image = cv2.merge([r,g,b])     # switch it to rgb
 	return rgb_image
-	
 
-for line in lines:
-	img_center = read_image(line[0],image_sizeX,image_sizeY)
-	img_left = read_image(line[1],image_sizeX,image_sizeY)
-	img_right = read_image(line[2],image_sizeX,image_sizeY)
-	images.append(img_center)
-	images.append(img_left)
-	images.append(img_right)
-	steer_angle_center = float(line[3])
-	measurements.append(steer_angle_center)
-	measurements.append(steer_angle_center + correction)
-	measurements.append(steer_angle_center - correction)
-augmented_images, augmented_measurements = [],[]
-for image,measurement in zip(images, measurements):
-    augmented_images.append(image)
-    augmented_measurements.append(measurement)
-    augmented_images.append(cv2.flip(image,1))
-    augmented_measurements.append(measurement*-1.0)
+def generator(samples, batch_size=32):
+	num_samples = len(samples)
+	while 1: # Loop forever so the generator never terminates
+		shuffle(samples)
+		for offset in range(0, num_samples, batch_size):
+			batch_samples = samples[offset:offset+batch_size]
+			images = []
+			angles = []
+			for batch_sample in batch_samples:
+				name = batch_sample[0]
+				#name = './IMG/'+batch_sample[0].split('/')[-1]
+				center_image = bgr2rgb(cv2.imread(name))              
+				#name = './IMG/'+batch_sample[1].split('/')[-1]
+				left_image = bgr2rgb(cv2.imread(name))
+				#name = './IMG/'+batch_sample[2].split('/')[-1]
+				right_image = bgr2rgb(cv2.imread(name))
+				center_angle = float(batch_sample[3])
+				images.append(center_image)
+				images.append(left_image)
+				images.append(right_image)
+				angles.append(center_angle)
+				angles.append(center_angle+correction)
+				angles.append(center_angle-correction)
+				images.append(cv2.flip(center_image,1))
+				images.append(cv2.flip(left_image,1))
+				images.append(cv2.flip(right_image,1))
+				angles.append(center_angle*-1.0)
+				angles.append((center_angle+correction)*-1.0)
+				angles.append((center_angle-correction)*-1.0)
 
+			# trim image to only see section with road
+			X_train = np.array(images)
+			y_train = np.array(angles)
+			yield sklearn.utils.shuffle(X_train, y_train)
 
-X_train = np.array(augmented_images)
-y_train = np.array(augmented_measurements)
+# compile and train the model using the generator function
+train_generator = generator(train_samples, batch_size=32)
+validation_generator = generator(validation_samples, batch_size=32)
 
 
 from keras.models import Sequential
@@ -51,26 +68,27 @@ from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 from keras.layers import Cropping2D
 
-
 model = Sequential()
-model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(image_sizeY,image_sizeX,3)))
-model.add(Cropping2D(cropping=((70,25), (0,0))))
+# Preprocess incoming data, centered around zero with small standard deviation 
+model.add(Cropping2D(cropping=((70,25), (0,0)), input_shape=(160,320,3)))
+model.add(Lambda(lambda x: x / 255.0 - 0.5))
 model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
 model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))
 model.add(Convolution2D(48,5,5,subsample=(2,2),activation="relu"))
 model.add(Convolution2D(64,3,3,activation="relu"))
 model.add(Convolution2D(64,3,3,activation="relu"))
+model.add(Dropout(0.5))
 model.add(Flatten())
 model.add(Dense(100))
 model.add(Dense(50))
 model.add(Dense(10))
 model.add(Dense(1))
 
-
-
 model.compile(loss='mse', optimizer='adam')
-history_object = model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5, verbose = 1)
-model.save('model.h5')
+history_object = model.fit_generator(train_generator, samples_per_epoch= \
+				len(train_samples), validation_data=validation_generator, \
+				nb_val_samples=len(validation_samples), nb_epoch=3)
+model.save('model.h5')	
 
 import matplotlib.pyplot as plt
 
@@ -85,3 +103,4 @@ plt.ylabel('mean squared error loss')
 plt.xlabel('epoch')
 plt.legend(['training set', 'validation set'], loc='upper right')
 plt.show()
+
